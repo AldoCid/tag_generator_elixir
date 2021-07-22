fb66a91ce99b02b281ccf5e62491ea6ec65ed379
AldoCid <aldocide@gmail.com>
2021-07-22 17:59:06 -0300

    🧪 tag experiment

--- lib/tag_generator_elixir.ex
+++ lib/tag_generator_elixir.ex
@@ -5,7 +5,15 @@ defmodule TagGeneratorElixir do
   Documentation for TagGeneratorElixir.
   """
 
-  def create_tag do
-    # Git.tag()
+  @repo %Git.Repository{
+    path: "/Users/aldo.cid/Desktop/Sumuprojects/HackDayz/tag_generator_elixir"
+  }
+
+  def config() do
+    # Git.re
+  end
+
+  def create_tag(repo, version, environment) do
+    Git.tag(@repo, "v{version}-environment")
   end
 end
