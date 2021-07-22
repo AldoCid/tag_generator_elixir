502cd9c80e20ea9ccec9c83bdcd658c3ecd75fdb
AldoCid <aldocide@gmail.com>
2021-07-22 19:09:16 -0300

    ✨ adds create_tag method

--- lib/tag_generator_elixir.ex
+++ lib/tag_generator_elixir.ex
@@ -13,7 +13,14 @@ defmodule TagGeneratorElixir do
     # Git.re
   end
 
-  def create_tag(repo, version, environment) do
-    Git.tag(@repo, "v{version}-environment")
+  @spec create_tag(%Git.Repository{}, String.t(), String.t()) :: IO.puts()
+  def create_tag(repo, version, environment_number) do
+    new_tag = "v#{version}-#{environment_number}"
+    IO.puts("Creating tag #{new_tag}...\n")
+
+    case Git.tag(repo, new_tag) do
+      {:ok, _} -> IO.puts("Tag #{new_tag} created with success!")
+      {:error, %Git.Error{message: message}} -> IO.puts(message)
+    end
   end
 end
