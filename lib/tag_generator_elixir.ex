1aea7cc4a2b9e0ce5548bcf076db4cbcd0faed2d
AldoCid <aldocide@gmail.com>
2021-07-22 16:20:44 -0300

    ✨ first commit

---
+++ lib/tag_generator_elixir.ex
@@ -0,0 +1,11 @@
+defmodule TagGeneratorElixir do
+  alias Git
+
+  @moduledoc """
+  Documentation for TagGeneratorElixir.
+  """
+
+  def create_tag do
+    # Git.tag()
+  end
+end
---
+++ test/tag_generator_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TagGeneratorElixirTest do
+  use ExUnit.Case
+  doctest TagGeneratorElixir
+
+  test "greets the world" do
+    assert TagGeneratorElixir.hello() == :world
+  end
+end
